e1ad328ff06f2c2268aa918c407a7bbcf5310f0f
GabrielRossi01 <rossi17006@gmail.com>
2025-08-14 10:18:03 -0300

    criando as classes Carta e Jogo

--- src/main/java/com/example/Carta.java
+++ src/main/java/com/example/Carta.java
@@ -10,7 +10,8 @@ public class Carta {
     }
 
     public String imagePath() {
-        return "classic-cards/" + numero + naipe + ".png";
+        String[] prefixos ={"0", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        return "classic-cards/" + prefixos[numero] + naipe + ".png";
     }
 
     public int getNumero() {
